cf1eb1d1716e558d589e813c5fda777d846a545f
MikeyMoMo <15792417+MikeyMoMo@users.noreply.github.com>
2024-01-19 13:46:29 +0800

    Update Defines_and_Docs.h

--- Defines_and_Docs.h
+++ Defines_and_Docs.h
@@ -15,8 +15,8 @@ String lonstr = "120.7671";  // Your longitude for Bangui
 
 // POSIX TimeZone rule for where you are.
 //const char* time_zone = "EST5EDT,M3.2.0/2:00:00,M11.1.0/2:00:00";  // for Buffalo (snow testing)
-//String latstr = "42.886447";   // Your latitude for Shreveport
-//String lonstr = "-78.878369";  // Your longitude for Shreveport
+//String latstr = "42.886447";   // Your latitude for Buffalo
+//String lonstr = "-78.878369";  // Your longitude for Buffalo
 
 // Get OneCall API key.
 //const String api_key = "Your_OWM_API_key_here";  // Get OneCall API key.
